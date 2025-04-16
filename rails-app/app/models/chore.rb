0f86851deb39204274a28cb49ce3c864136d8526
mariaaxime <mariaximenaam@gmail.com>
2025-04-16 20:37:06 +1000

    Annotate models

--- rails-app/app/models/chore.rb
+++ rails-app/app/models/chore.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: chores
+#
+#  id           :integer          not null, primary key
+#  name         :string           not null
+#  schedule     :json             not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  household_id :integer          not null
+#
+# Indexes
+#
+#  index_chores_on_household_id  (household_id)
+#
+# Foreign Keys
+#
+#  household_id  (household_id => households.id)
+#
 class Chore < ApplicationRecord
   belongs_to :household
 end
--- rails-app/app/models/household.rb
+++ rails-app/app/models/household.rb
@@ -1,2 +1,10 @@
+# == Schema Information
+#
+# Table name: households
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Household < ApplicationRecord
 end
